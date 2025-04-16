9f8f20d371d694e983e892ec302f3f9ad4c8f169
Germán Herrera <germanh1982@gmail.com>
2025-04-16 18:15:15 -0300

    feat: parametrize bridge configuration with config file.

--- bridge/main.py
+++ bridge/main.py
@@ -3,39 +3,44 @@ import json
 from influxdb_client import InfluxDBClient, Point
 import logging
 import signal
-
-INFLUXDB_TOKEN = 'aqece41MRt-wGiKkdY7A7n1FPEMAj7ahiZVPD8_TEAKvBHfCPkLxQIf7RoNrG3dGEYECSuSZQZP0GD-Q8bxe0w=='
-INFLUXDB_BUCKET = 'home'
-INFLUXDB_ORG = 'home'
-INFLUXDB_URL = 'http://influxdb:8086'
+import yaml
+from argparse import ArgumentParser
+import sys
 
 def on_subscribe(client, userdata, mid, reason_code_list, properties):
     log.info("Subscribed")
 
 def on_connect(client, write_api, connect_flags, reason_code, properties):
     log.info("Connected to broker")
-    client.subscribe('environmental/#')
+    client.subscribe(cfg['mqtt']['receive_topic'])
 
 def on_message(client, write_api, message):
     log.info(f"{message.topic} payload={json.loads(message.payload)}")
     topic_components = message.topic.split('/')
     msgdict = json.loads(message.payload)
-    point = Point('environmental').tag('devid', topic_components[-1]).field("temp", msgdict['temp']).field("hum", msgdict['hum'])
-    write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=point)
+    point = Point(cfg['data']['measurement_name']).tag('devid', topic_components[-1]).field("temp", msgdict['temp']).field("hum", msgdict['hum'])
+    write_api.write(bucket=cfg['influxdb']['bucket'], org=cfg['influxdb']['org'], record=point)
 
 def main():
-    write_client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+    write_client = InfluxDBClient(
+            url=cfg['influxdb']['url'],
+            token=cfg['influxdb']['token'],
+            org=cfg['influxdb']['org'])
     write_api = write_client.write_api()
 
     try:
-        client = mqttc.Client(mqttc.CallbackAPIVersion.VERSION2, userdata=write_api)
+        client = mqttc.Client(
+                mqttc.CallbackAPIVersion.VERSION2,
+                userdata=write_api)
         client.on_message = on_message
         client.on_connect = on_connect
-        client.connect(host='mosquitto')
+        client.connect(host=cfg['mqtt']['host'])
 
         def sigint_handler(signum, stack):
             log.info("SIGINT received")
             client.loop_stop()
+            log.debug("MQTT client stopped")
+            sys.exit(0)
 
         signal.signal(signal.SIGINT, sigint_handler)
 
@@ -48,6 +53,13 @@ def main():
         log.info("Aborted by user")
 
 if __name__ == '__main__':
+    p = ArgumentParser()
+    p.add_argument('configfile')
+    args = p.parse_args()
     logging.basicConfig(style='{', format='{asctime} {name} {levelname} {msg}', level=logging.INFO)
     log = logging.getLogger(__name__)
+
+    with open(args.configfile) as cfh:
+        cfg = yaml.safe_load(cfh.read())
+
     main()
